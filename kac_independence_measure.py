5cc06268ee55e69dc16a7ee3ca6e2a7d2cac3fd5
tank <povilas.daniusis@gmail.com>
2021-12-11 14:51:09 +0200

    added article draft

--- kac_independence_measure.py
+++ kac_independence_measure.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from torch.autograd import Variable
 import itertools
 from torch.nn.utils import weight_norm
+import numpy as np
 
 from sklearn.datasets import load_boston
 from sklearn.preprocessing import StandardScaler
@@ -65,15 +66,15 @@ class KacIndependenceMeasure(nn.Module):
 
 
 if __name__ == "__main__":    
-    n_batch = 1024 #1024
-    dim_x = 512
-    dim_y = 4
-    num_iter = 2000 #1300
-    input_proj_dim = 64
+    n_batch = 2048 #1024
+    dim_x = 1024
+    dim_y = 32
+    num_iter = 500 #1300
+    input_proj_dim = 0
 
     model = KacIndependenceMeasure(dim_x, dim_y, lr=0.05, num_iter = num_iter, input_projection_dim = input_proj_dim)
     
-  
+    """
     # inedependent data
     print("Independent data")
     history_indep = []
@@ -83,9 +84,8 @@ if __name__ == "__main__":
         dep = model(x,y)
         history_indep.append(dep.detach().numpy())
         #print("{} {}".format(i, dep))
-    plt.plot(history_indep)
-    plt.show()
-    
+    plt.plot(history_indep, label='Independent')
+
     
 
     model.reset()    
@@ -101,17 +101,48 @@ if __name__ == "__main__":
         noise = torch.randn(n_batch, dim_y)
         #y = (torch.sin(proj_x) + torch.cos(proj_x))*noise    
         #y = torch.log(1.0 + torch.abs(proj_x))
-        y = torch.sin(proj_x) + torch.cos(proj_x)  + 0.15*noise
+        y = torch.sin(proj_x) + torch.cos(proj_x)  + 0.2*noise
+        #y = x
         dep = model(x,y)
         history_dep.append(dep.detach().numpy())
         #print("{} {}".format(i, dep))
-    plt.plot(history_dep)
-    plt.show()
+    plt.plot(history_dep, label="Dependent")
+    plt.savefig('./additive_noise_effect_0.1.png')
+    """
 
-    
-    
+    file_object = open('noise_effect.txt', 'w')
 
+    to_plot = []
+    step = 0
+    for noise_level in np.arange(0, 2.5, 0.1):
+        print("Noise level: {}".format(noise_level))
+        history_dep = []
+        random_proj = nn.Linear(dim_x, dim_y)
+        model.reset()
+        for i in range(num_iter):
+            x = torch.randn(n_batch, dim_x)
+            proj_x = random_proj(x)
+            noise = torch.randn(n_batch, dim_y)
+            #y = (torch.sin(proj_x) + torch.cos(proj_x))*noise    
+            #y = torch.log(1.0 + torch.abs(proj_x))
+            y = torch.sin(proj_x) + torch.cos(proj_x)  + noise_level*noise
+            #y = x
+            dep = model(x,y)
+            history_dep.append(dep.detach().numpy())
+            #print("{} {}".format(i, dep))
+        to_plot.append(history_dep[-1])
+        file_object.write(str(history_dep[-1]))    
+        file_object.write('\n')
+        plt.plot(history_dep, label="Dependent")
+        plt.savefig('./additive_noise_effect_{}.png'.format(step))
+        step = step + 1
+        plt.clf()
 
+    breakpoint()
+    plt.plot(np.arange(0, 2.5, 0.1), to_plot)    
+    plt.savefig('./additive_noise_effects.png')
+
+    file_object.close()
 
 
 
