7072e133d82dbd0e0bd46794a082803f5c0e1a28
tank <povilas.daniusis@gmail.com>
2022-01-20 11:27:06 +0200

    feature extraction experiment: added dataset loader

--- experiments/feature_extraction.py
+++ experiments/feature_extraction.py
@@ -18,11 +18,16 @@ from kac_independence_measure import KacIndependenceMeasure
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.datasets import load_digits
 from sklearn import datasets, neighbors, linear_model
+from uci_loader import UCIDatasets
 
 if __name__ == "__main__":
 
     if not os.path.exists('feature_extraction'):
         os.makedirs('feature_extraction')
+
+    datasets = UCIDatasets('energy', n_splits=2)
+
+
 
     n_batch = 256 #2048
     dim_x = 64 # 1024
@@ -30,32 +35,12 @@ if __name__ == "__main__":
     num_iter = 500 #500
     input_proj_dim = 0 #0
 
-    X_digits, y_digits = datasets.load_digits(return_X_y=True)
-    X_digits = X_digits / X_digits.max()
-
-    n_samples = len(X_digits)
-
-    X_train = X_digits[: int(0.9 * n_samples)]
-    y_train = y_digits[: int(0.9 * n_samples)]
-    X_test = X_digits[int(0.9 * n_samples) :]
-    y_test = y_digits[int(0.9 * n_samples) :]
-
-    model = KacIndependenceMeasure(dim_x, dim_y, lr=0.05, num_iter = num_iter, input_projection_dim = input_proj_dim)
+    model = KacIndependenceMeasure(dim_x, dim_y, lr=0.05, input_projection_dim = input_proj_dim, weight_decay=0.01)
     knn = KNeighborsClassifier(n_neighbors=3)
     logistic = linear_model.LogisticRegression(max_iter=1000)
-    #knn.fit(X_train, y_train)
 
-    print("Raw data")
-    print("KNN score: %f" % knn.fit(X_train, y_train).score(X_test, y_test))
-    print(
-        "LogisticRegression score: %f"
-        % logistic.fit(X_train, y_train).score(X_test, y_test)
-    )
-    breakpoint()
-
-    num_iter = 1024
-
-    #for i in range(num_iter):
-    #    model.forward(Xb, Yb)
-
+    train_data = datasets.get_split(0)
+    test_data = datasets.get_split(1)
+    print(train_data)
+    print(test_data)
     
