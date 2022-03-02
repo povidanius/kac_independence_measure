0974681f15c01af4fd83bbd1b57f7b7ca8df9d65
tank <povilas.daniusis@gmail.com>
2022-03-02 19:56:42 +0200

    article updates

--- experiments/chest_classifier.py
+++ experiments/chest_classifier.py
@@ -9,11 +9,42 @@ from torchvision.models import inception_v3, resnet18
 from torch import optim
 import sys
 import os
+from collections import OrderedDict
+
 
 data_path='/home/tank/Downloads/chest_xray/chest_xray'
 
 sys.path.insert(0, "../")
 from kac_independence_measure import KacIndependenceMeasure
+from torch.nn.functional import *
+
+class ResNet18(nn.Module):
+    
+    def __init__(self, layer_numbers):
+        super().__init__()
+        self.layer_numbers = layer_numbers # indices of layers whose activations are needed
+        self.layer_acts = OrderedDict()
+        #self.resnet_18 = inception_v3(pretrained=True, aux_logits=False) 
+        self.resnet_18 = resnet18(pretrained=True) #, aux_logits=True)
+        self.forward_hooks = []
+        
+        #register hooks
+        for layer_num, layer_name in enumerate(self.resnet_18._modules.keys()):
+            if layer_num in layer_numbers:
+                print("Layer num {}, name: {}, shape {}".format(layer_num, layer_name, ''))
+                self.forward_hooks.append(getattr(self.resnet_18, layer_name).register_forward_hook(self.get_activations(layer_name)))
+    
+    #Defining hook to get intermediate features     
+    def get_activations(self, layer_name):
+        def hook(module, input, output):
+            self.layer_acts[layer_name] = output
+        return hook
+    
+    #forward pass
+    def forward(self, x):
+        out = self.resnet_18(x)
+        #don't need output values
+        return self.layer_acts
 
 
 #os.listdir('./chest-xray-pneumonia/')
@@ -49,9 +80,12 @@ train_loader = DataLoader(dataset=training_dataset, batch_size=64, shuffle=True)
 val_loader = DataLoader(dataset=validation_dataset, shuffle= True)
 test_loader = DataLoader(dataset= testing_dataset, shuffle=False)
 
+z_key = 2
+
+
 #model = inception_v3(pretrained=True, aux_logits=False)
-model = resnet18(pretrained=True) #, aux_logits=False)
-#features = nn.Sequential(*(list(resnet18.children())[0:8]))
+required_layers = [1, z_key, 17]
+model = ResNet18(layer_numbers=required_layers) #pretrained=True) #, aux_logits=False)
 
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -65,6 +99,9 @@ test_loss = []
 train_accuracy = []
 test_accuracy = []
 
+num_kac_iters = 200
+optimize_kac_every_iters = 500
+
 for epoch in range(number_of_epoch):
     
     train_correct = 0
@@ -75,6 +112,7 @@ for epoch in range(number_of_epoch):
     test_iteration = 0
     
     model.train()
+    iteration = 0
     for data,label in train_loader:
         
         optimizer.zero_grad()
@@ -82,7 +120,8 @@ for epoch in range(number_of_epoch):
         label = label.to(device)
         
         pred = model(data)
-        loss = loss_fn(pred, label)
+        breakpoint()
+        loss = loss_fn(pred[0], label)
         loss.backward()
         optimizer.step()
         
@@ -91,6 +130,11 @@ for epoch in range(number_of_epoch):
         
         _, predicted = torch.max(pred, 1)
         train_correct += (predicted == label).sum()
+        iteration = iteration + 1
+
+        if iteration % optimize_kac_every_iters == 0:
+            # optimize kac_im
+            pass
         
     train_loss.append(train_iter_loss/train_iteration)
     train_accuracy.append(100*float(train_correct)/len_train)
